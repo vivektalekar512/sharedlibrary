8a9a1abdaa520876c7083423a9c09b3dfbc9479c
vivektalekar512 <126253813+vivektalekar512@users.noreply.github.com>
2023-04-24 18:03:02 +0530

    Update assignment.groovy

--- vars/assignment.groovy
+++ vars/assignment.groovy
@@ -1,7 +1,5 @@
-def call() {
-  def currentUser = currentBuild.getBuildCauses()[0].getUserId()
-  def currentJob = env.JOB_NAME
-  echo "Current user: ${currentUser}"
-  echo "Current job: ${currentJob}"
-  echo "Var email: ${var_email}"
+def call(String var_email) {
+    println("var_email: ${var_email}")
+    println("Current login user: ${env.USER}")
+    println("Current job name: ${env.JOB_NAME}")
 }
